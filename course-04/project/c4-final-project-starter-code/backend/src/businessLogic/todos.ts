55adcdfc89fa11e473be0be0e75994191d3e61bc
Edidiong Udoh(TechnicalDee) <gisthive@gmail.com>
2022-08-29 00:03:00 +0100

    frontend and minor

--- course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/businessLogic/todos.ts
@@ -1,4 +1,4 @@
-import { TodosAccess } from './todosAcess'
+import { TodosAccess } from '../dataLayer/todosAcess'
 // import { AttachmentUtils } from './attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
