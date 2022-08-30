653ded6e2b9329577935fecbe6d08ce61c07bfe8
Edidiong Udoh <42146638+technicaldee@users.noreply.github.com>
2022-08-30 09:25:24 +0100

    Update config.ts

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '9vgrbj77ke'
+const apiId = '9grj77ke'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
